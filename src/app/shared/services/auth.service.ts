cf96e84215212bd8be9be3ccdefc4986d40a4675
Katie Guill <Katie.Guill@vu.com>
2017-10-18 17:39:36 -0500

    environment setup

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class AuthService {
   private options = new RequestOptions({ withCredentials: true });
+  private url: string = `${environment.apiBaseUrl}/user`;
   
-  constructor(private http: Http) {
-    
-  }
+  constructor(private http: Http) { }
 
   login(email: string, password: string): Observable<boolean | Response> {
     const loginInfo = { 'email': email, 'password': password };
-    return this.http.put("https://dj-sails-todo.azurewebsites.net/user/login", loginInfo, this.options)
+    return this.http.put( `${this.url}/login`, loginInfo, this.options)
         .do((res: Response) => {
             if (res){
                 return Observable.of(true);
@@ -28,7 +28,7 @@ export class AuthService {
 
   signup(email: string, password: string): Observable<boolean | Response> {
     const loginInfo = { 'email': email, 'password': password };
-    return this.http.post("https://dj-sails-todo.azurewebsites.net/user/", loginInfo, this.options)
+    return this.http.post(this.url, loginInfo, this.options)
       .do((res: Response) => {
         if (res) {
           return Observable.of(true);
@@ -43,7 +43,7 @@ export class AuthService {
    }
 
    isAuthenticated(): Observable<boolean> {
-    return this.http.get("https://dj-sails-todo.azurewebsites.net/user/identity", this.options)
+    return this.http.get(`${this.url}/identity`, this.options)
       .map((res: Response) => {
         if (res) {
           return Observable.of(true);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  environmentName: 'Development',
+  apiBaseUrl: 'https://dj-sails-todo.azurewebsites.net'
 };
