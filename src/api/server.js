81019aa996fab36ee9380e5f7c0101e12069636a
Katie Guill <Katie.Guill@vu.com>
2017-10-24 07:30:54 -0500

    change folder structure, add folder for api

--- src/api/server.js
+++ src/api/server.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
   res.send('LibWay');
 })
 
-app.get('/listBooks', function (req, res) {	
+app.get('/listBooks', function (req, res) {
 	res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
 	res.header('Access-Control-Allow-Credentials', 'true');
 	
@@ -14,6 +14,20 @@ app.get('/listBooks', function (req, res) {
        console.log( data );
        res.end( data );
    });
+})
+
+app.post('/search', function (req, res) {
+	res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  //TODO:
+  //replace below with call to Sierra API and return back list of books
+  //see src/app/classes/book.ts for reference (return those things for each book)
+  fs.readFile( __dirname + "/" + "books.json", 'utf8', function (err, data) {
+    console.log( data );
+    res.end( data );
+});
+
 })
 
 var server = app.listen(8081, function () {
